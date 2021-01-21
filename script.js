41015ee5e9cd5dfb82bca9c62a150c36560166eb
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-01-21 13:04:33 +0200

    Adding array with 10 objects
    object includes:
    1. Start time.
    2. End time.
    3. Tasks given.
    4. Tasks finished.
    5. Topic.

---
+++ script.js
@@ -0,0 +1,113 @@
+const toDoList = [
+    {
+        startedAt: new Date("2021-01-20:13:00"),
+
+        finishedAt: new Date("2021-01-20:19:00"),
+
+        tasksGiven: 10,
+
+        tasksFinished: 7,
+
+        topic: HTML
+    },
+    {
+        startedAt: new Date("2021-01-20:15:00"),
+
+        finishedAt: new Date("2021-01-20:18:00"),
+
+        tasksGiven: 5,
+
+        tasksFinished: 4,
+
+        topic: JavaScript
+    },
+    {
+        startedAt: new Date("2021-01-20:14:00"),
+
+        finishedAt: new Date("2021-01-20:18:00"),
+
+        tasksGiven: 6,
+
+        tasksFinished: 6,
+
+        topic: css
+    },
+    {
+        startedAt: new Date("2021-01-20:10:00"),
+
+        finishedAt: new Date("2021-01-20:12:00"),
+
+        tasksGiven: 4,
+
+        tasksFinished: 3,
+
+        topic: Arrays
+    },
+    {
+        startedAt: new Date("2021-01-20:09:00"),
+
+        finishedAt: new Date("2021-01-20:10:00"),
+
+        tasksGiven: 7,
+
+        tasksFinished: 5,
+
+        topic: loops
+    },
+    {
+        startedAt: new Date("2021-01-20:12:30"),
+
+        finishedAt: new Date("2021-01-20:17:00"),
+
+        tasksGiven: 12,
+
+        tasksFinished: 11,
+
+        topic: Functions
+    },
+    {
+        startedAt: new Date("2021-01-20:11:00"),
+
+        finishedAt: new Date("2021-01-20:15:00"),
+
+        tasksGiven: 15,
+
+        tasksFinished: 10,
+
+        topic: Objects
+    },
+    {
+        startedAt: new Date("2021-01-20:14:00"),
+
+        finishedAt: new Date("2021-01-20:16:30"),
+
+        tasksGiven: 15,
+
+        tasksFinished: 13,
+
+        topic: Strings
+    },
+    {
+        startedAt: new Date("2021-01-20:15:00"),
+
+        finishedAt: new Date("2021-01-20:17:30"),
+
+        tasksGiven: 16,
+
+        tasksFinished: 16,
+
+        topic: Variables
+    },
+    {
+        startedAt: new Date("2021-01-20:17:00"),
+
+        finishedAt: new Date("2021-01-20:19:30"),
+
+        tasksGiven: 4,
+
+        tasksFinished: 3,
+
+        topic: Classes
+    },
+]
+
